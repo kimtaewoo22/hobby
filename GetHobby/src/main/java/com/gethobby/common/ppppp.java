66bed27fa12d8c6d4e9101785e14a3a5b35f8bf0
user <user@DESKTOP-74IMAPF>
2020-01-22 17:54:56 +0900

    cococo

---
+++ GetHobby/src/main/java/com/gethobby/common/ppppp.java
@@ -0,0 +1,14 @@
+package com.gethobby.common;
+
+public class ppppp {
+
+	public ppppp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
